3f6380032d32ab162dd58eb894ef745e10ddebbd
Mariusz89B <mariusz89b@mods-kodi.pl>
2022-06-11 18:34:29 +0200

    v1.0.5

--- addon.py
+++ addon.py
@@ -2067,7 +2067,7 @@ def build_m3u():
 
         data += '\n#EXTINF:-1 tvg-id="{id}" tvg-name="{title}" tvg-logo="{icon}" group-title="C More", {title}\n{url}'.format(id=tvg_id, title=title, url=url, icon=icon)
 
-    with open(path + 'cmore_iptv.m3u', 'w+') as f:
+    with open(path + 'cmore_iptv.m3u', 'w+', encoding='utf-8') as f:
         f.write(data)
 
     xbmcgui.Dialog().notification(localized(30012), localized(30064), xbmcgui.NOTIFICATION_INFO)
